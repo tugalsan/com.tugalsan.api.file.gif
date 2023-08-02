66047171d7564a56a01a2247e9610dbbe6e2c6f5
tugalsan karabacak <tugalsan@gmail.com>
2023-08-02 10:14:08 +0300

    .

--- src/main/java/com/tugalsan/api/file/gif/server/TS_FileGifWriter.java
+++ src/main/java/com/tugalsan/api/file/gif/server/TS_FileGifWriter.java
@@ -34,7 +34,7 @@ public class TS_FileGifWriter implements AutoCloseable {
         return closed || writerBall != null;
     }
 
-    public boolean accept(RenderedImage img) {
+    public boolean add(RenderedImage img) {
         if (img == null) {
             return false;
         }
